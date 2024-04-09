80e4f1814223764099d4c6b38ad07c67c2128f06
RayChaun <rraglin0@email.cpcc.edu>
2024-04-08 23:48:32 -0400

    "Modified to read input from a file"
    I failed to figure out how to properly use arrays among other things.

--- Policy.java
+++ Policy.java
@@ -1,7 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Policy
 {  
    //fields
-   private String policyNumber;
+   private int policyNumber;
    private String providerName;
    private String firstName;
    private String lastName;
@@ -13,7 +17,7 @@ public class Policy
    //constructors
    public Policy()
    {
-      policyNumber = "";
+      policyNumber = 0;
       providerName = "";
       firstName = "";
       lastName = "";
@@ -23,7 +27,7 @@ public class Policy
       weight = 0;
    }
    
-   public Policy(String pNumber, String pName, String fName, String lName,int a, String sStatus, double h, double w)
+   public Policy(int pNumber, String pName, String fName, String lName,int a, String sStatus, double h, double w)
    {
       policyNumber = pNumber;
       providerName = pName;
@@ -40,7 +44,7 @@ public class Policy
    /*
    @param pNumber What the policy number wil be set to
    */
-   public void setPolicyNumber(String pNumber)
+   public void setPolicyNumber(int pNumber)
    {
       policyNumber = pNumber;
    }
@@ -107,7 +111,7 @@ public class Policy
    /*
    @return The policy number
    */
-   public String getPolicyNumber()
+   public int getPolicyNumber()
    {
       return policyNumber;
    }
@@ -207,6 +211,85 @@ public class Policy
          price += ((getBMI() - BMI_THRESHOLD) * ADDITIONAL_FEE_PER_BMI); //additional BMI fee - 20
          
       return price;
+   }
+   
+   //method to display policy information
+   public static void displayPolicyInformation(Policy policy) 
+   {
+      System.out.println("Policy Number: " + policy.getPolicyNumber());
+      System.out.println("Provider Name: " + policy.getProviderName());
+      System.out.println("Policyholder's First Name: " + policy.getFirstName());
+      System.out.println("Policyholder's Last Name: " + policy.getLastName());
+      System.out.println("Policyholder's Age: " + policy.getAge());
+      System.out.println("Policyholder's Smoking Status (smoker/non-smoker): " + policy.getSmokingStatus());
+      System.out.println("Policyholder's Height: " + policy.getHeight() + " inches");
+      System.out.println("Policyholder's Weight: " + policy.getWeight() + " pounds");
+      System.out.println("Policyholder's BMI: " + policy.getBMI());
+      System.out.println("Policy Price: $" + policy.getPrice());
+      System.out.println();
+    }
+   
+   //method for counting the number of smokers
+   public static int countSmokers(Policy[] policies) 
+   {
+      int count = 0;
+      
+      for (Policy policy : policies) 
+      {
+          if (policy.getSmokingStatus().equals("smoker")) 
+          {
+             count++;
+          }
+      }
+      return count;
+   }
+   
+   //method to read policy information from file
+   public static Policy[] readPoliciesFromFile(String filename) 
+   {
+      Policy[] policies = null;
+      try 
+      {
+          File file = new File(filename);
+          Scanner scanner = new Scanner(file);
+
+          int policyCount = 0;
+          while (scanner.hasNextLine()) 
+          {
+              scanner.nextLine();
+              policyCount++;
+          }
+
+          scanner.close();
+
+          policies = new Policy[policyCount];
+          scanner = new Scanner(file);
+
+          int index = 0;
+          while (scanner.hasNextLine()) 
+          {
+              int policyNumber = Integer.parseInt(scanner.nextLine());
+              String providerName = scanner.nextLine();
+              String firstName = scanner.nextLine();
+              String lastName = scanner.nextLine();
+              int age = Integer.parseInt(scanner.nextLine());
+              String smokingStatus = scanner.nextLine();
+              double height = Double.parseDouble(scanner.nextLine());
+              double weight = Double.parseDouble(scanner.nextLine());
+
+              policies[index] = new Policy(policyNumber, providerName, firstName, lastName, age, smokingStatus, height, weight);
+
+              index++;
+          }
+
+          scanner.close();
+      } 
+      catch (FileNotFoundException e) 
+      {
+          e.printStackTrace();
+      }
+
+      return policies;
    }
    
 }
